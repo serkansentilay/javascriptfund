0cd8091604ff5739604dd5940dd2609fcf9616d1
serkansentilay <sentiilay@gmail.com>
2025-07-21 15:13:46 +0300

    variables.js updated

--- variables.js
+++ variables.js
@@ -272,12 +272,17 @@
 
 
 //let – is a modern variable declaration.
-//var – is an old-school variable declaration. Normally we don’t use it at all, but we’ll cover subtle differences from let in the chapter The old "var", just in case you need them.
+//var – is an old-school variable declaration. Normally we don’t use it at all, 
+// but we’ll cover subtle differences from let in the chapter The old "var", just in case you need them.
 //const – is like let, but the value of the variable can’t be changed.
 
 
 //let and const are block-scoped, var is function-scoped.
 //let and const are not hoisted, var is hoisted.    
+//Hoisting, JavaScript’in çalışma zamanında değişken ve fonksiyon bildirimlerini 
+// kodun en üstüne "taşıması" (hoist etmesi) anlamına gelir.
+
+
 
 //let and const can’t be re-declared in the same scope, var can.    
 //let and const can’t be used before declaration, var can.
